ebb909937ae52c50a272e3499b0ba7da8298c565
meliodas0n <mrunalnarayana@gmail.com>
2023-06-03 23:17:43 +0530

    cleaned up

--- app.py
+++ app.py
@@ -6,19 +6,13 @@ import os
 import config
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(config.APP_SETTINGS)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
-
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def home():
   return render_template("home.html")
-
-@app.route("/<name>")
-def hello_name(name):
-  return render_template("home.html", name = name)
 
 if __name__ == "__main__":
   app.run(host = config.HOST, port = config.PORT, debug = True, threaded = True)
--- config.py
+++ config.py
@@ -3,13 +3,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 HOST = "127.0.0.1"
 PORT = 5123
+APP_SETTINGS="config.DevelopmentConfig"
+DATABASE_URL="postgresql:///flask"
 
 class Config:
   DEBUG = False
   DEVELOPMENT = False
   THREADED = True
   SECRET_KEY = "queuework"
-  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+  SQLALCHEMY_DATABASE_URI = DATABASE_URL 
   
 class ProductionConfig(Config):
   DEBUG = False
