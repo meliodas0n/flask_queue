d453e4058839646e83051f2c7bb2647745d023b3
meliodas0n <mrunalnarayana@gmail.com>
2023-06-03 23:47:10 +0530

    unable to write to json

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ import config
 import requests
 import os
 import operator
-
+import re
+from collections import Counter
+import nltk
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+import json
 
 app = Flask(__name__)
 app.config.from_object(config.APP_SETTINGS)
+result_file = 'result.json'
+RESULTS = []
 
 @app.route("/", methods = ['GET', 'POST'])
 def home():
@@ -22,6 +29,37 @@ def home():
       print(r.text)
     except:
       errors.append("Unable to get URL. Please make sure it's valid and try again...")
+    if r:
+      raw = BeautifulSoup(r.text, 'html.parser').get_text()
+      nltk.data.path.append('./nltk_data/')
+      tokens = nltk.word_tokenize(raw)
+      text = nltk.Text(tokens)
+      nonPunct = re.compile('.*[A-Za-z].*')
+      raw_words = [w for w in text if nonPunct.match(w)]
+      raw_word_count = Counter(raw_words)
+      no_stop_words = [w for w in raw_words if w.lower() not in stopwords.words('english')]
+      no_stop_words_count = Counter(no_stop_words)
+      results = sorted(
+        no_stop_words_count.items(),
+        key = operator.itemgetter(1),
+        reverse = True
+      )
+    print(results)
+    try:
+      if os.path.isfile(result_file) is False:
+        with open(result_file, 'a') as f:
+          RESULTS = json.load(f)
+          RESULTS.append(results)
+          json.dump(RESULTS, result_file, indent = 2, separators = (',', ' : '))
+          f.close()
+      else:
+        with open(result_file, 'a') as f:
+          RESULTS = json.load(f)
+          RESULTS.append(results)
+          json.dump(RESULTS, result_file, indent = 2, separators = (',', ' : '))
+          f.close()
+    except Exception as e:
+      print(f"ERROR - {e}")
   return render_template("home.html", errors = errors, results = results)
 
 if __name__ == "__main__":
