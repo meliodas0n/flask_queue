df05b19b34cabd5d45f9539616f3e6510a807152
meliodas0n <mrunalnarayana@gmail.com>
2023-06-03 23:00:53 +0530

    setup some stuff

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 import os
 import config
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route("/")
 def home():
-  secret_key = app.config.get("SECRET_KEY")
-  return render_template("home.html", secret = secret_key) 
+  return render_template("home.html")
+
+@app.route("/<name>")
+def hello_name(name):
+  return render_template("home.html", name = name)
 
 if __name__ == "__main__":
   app.run(host = config.HOST, port = config.PORT, debug = True, threaded = True)
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 HOST = "127.0.0.1"
 PORT = 5123
@@ -7,10 +8,11 @@ class Config:
   DEBUG = False
   DEVELOPMENT = False
   THREADED = True
-  SECRET_KEY = os.getenv("SECRET_KEY", "automation")
+  SECRET_KEY = "queuework"
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
   
 class ProductionConfig(Config):
-  pass
+  DEBUG = False
 
 class DevelopmentConfig(Config):
   DEBUG = True
